83ab652a2d4afd851bd5446dcecaeba5af1c50c0
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2020-10-13 18:17:05 +0300

    new file:   index.html new file:   sandbox.js

---
+++ sandbox.js
@@ -0,0 +1,53 @@
+// alert('hello, world');
+
+// console.log(1);
+// console.log(2);
+
+///////////////////////////////////////////
+// let age = 26;
+// console.log(age);
+
+// age = 27;
+// console.log(age);
+
+// const score = 100;
+//score = 150;
+
+///////////////////////////////////////////
+// let points;
+// myAlert = () => {
+//     alert('hello');
+// }
+// function myAlertSecond() {
+//     alert('hello second');
+// }
+// console.log(typeof(1));
+// console.log(typeof('hello, world'));
+// console.log(typeof(true));
+// console.log(typeof(null));
+// console.log(typeof(points));
+// console.log(typeof(myAlert));
+// console.log(typeof(myAlertSecond));
+
+///////////////////////////////////////////
+
+console.log('hello');
+
+let email = 'myemail@mail.com';
+console.log(email);
+
+let firtsName = 'Jonh';
+let secondName = 'Deer';
+let fullName = firtsName + ' ' + secondName;
+console.log(fullName);
+
+console.log(fullName[1]);
+
+console.log(fullName.length);
+console.log(fullName.toUpperCase());
+let fullNameInLowerCase = fullName.toLowerCase();
+console.log(fullNameInLowerCase, fullName);
+
+let index = email.indexOf('@');
+console.log(index);
+
