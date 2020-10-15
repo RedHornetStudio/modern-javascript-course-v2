242d3f331a2669d050ea07ce9612cc8ce3337474
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2020-10-15 14:58:06 +0300

    modified:   sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -1,21 +1,50 @@
-function greet() {
-    console.log('Hello world');
-}
+// function greet() {
+//     console.log('Hello world');
+// }
 
-const speak = function() {
-    console.log('good day');
+// const speak = function() {
+//     console.log('good day');
+// };
+
+// const talk = () => {
+//     console.log('talking');
+// }
+
+// greet();
+// console.dir(greet);
+// console.log(greet);
+// speak();
+// console.dir(speak);
+// console.log(speak);
+// talk();
+// console.dir(talk);
+// console.log(talk);
+
+const greet = (name, time) => {
+    console.log(Boolean(!name));
+    console.log(Boolean(!time))
+    if(!name && !time) {
+        console.log('Hello');
+    } else if(!name) {
+        console.log(`Good ${time}`);
+    } else if(!time) {
+        console.log(`Hello ${name}`);
+    } else {
+        console.log(`Good ${time} ${name}`);
+    }
 };
 
-const talk = () => {
-    console.log('talking');
-}
-
 greet();
-console.dir(greet);
-console.log(greet);
-speak();
-console.dir(speak);
-console.log(speak);
-talk();
-console.dir(talk);
-console.log(talk);
+greet('Mario', 'day', 'hhh');
+greet(null, 'night');
+greet('Luigi');
+
+const price = (items, tax) => {
+    let total = 0;
+    items.forEach(element => {
+        total += element + element * tax;
+    });
+    return total
+};
+
+console.log(price([100, 1000], 0.2));
