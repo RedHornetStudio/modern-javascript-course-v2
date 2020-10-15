d2bd53772b977641f2bc94102b280ca3d8c8bac0
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2020-10-15 09:47:32 +0300

    modified:   sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -11,3 +11,18 @@ while(i < names.length) {
     console.log(html);
     i++;
 }
+
+let scores = [50, 30, 10, 0, 100, 30, 60];
+
+for(let i = 0; i < scores.length; i++) {
+    if(scores[i] === 0) {
+        continue;
+    }
+
+    if(scores[i] === 100) {
+        console.log('you get maximum score', scores[i]);
+        break;
+    }
+
+    console.log('you score is', scores[i]);
+}
