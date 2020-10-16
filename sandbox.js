0b92d1f260fd6bebd3facc65471ddcb6a7d199fa
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2020-10-16 18:53:14 +0300

    modified:   index.html modified:   sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -1,34 +1,60 @@
-const para = document.querySelector('p.error');
+// const para = document.querySelector('p.error');
 
-console.log(para);
+// console.log(para);
 
-const paras = document.querySelectorAll('p');
+// const paras = document.querySelectorAll('p');
 
-console.log(paras);
+// console.log(paras);
 
-paras.forEach(element => {
-    console.log(element);
+// paras.forEach(element => {
+//     console.log(element);
+// });
+
+// const div = document.querySelector('div');
+// console.log(div.innerText);
+
+// const content = document.querySelector('.content');
+
+// content.innerHTML += '<h1>this is content</h1>';
+
+// const link = document.querySelector('a');
+// console.log(link.getAttribute('href'));
+// link.setAttribute('href', 'https://www.thenetninja.co.uk/');
+// console.log(link.getAttribute('href'));
+
+// const paragraphs = document.querySelectorAll('.test > p');
+// console.log(paragraphs);
+
+// paragraphs.forEach(element => {
+//     if(element.textContent.includes('error')) {
+//         element.classList.add('error');
+//     } else if(element.textContent.includes('success')) {
+//         element.classList.add('success');
+//     }
+// });
+
+//////////////////////////////////////////////////////////////////
+
+const ul = document.querySelector('ul');
+
+ul.addEventListener('click', e => {
+    if(e.target.tagName === 'LI') {
+        e.target.remove();
+    }
 });
 
-const div = document.querySelector('div');
-console.log(div.innerText);
+const button = document.querySelector('.prepend');
 
-const content = document.querySelector('.content');
+button.addEventListener('click', () => {
+    const li = document.createElement('li');
+    li.textContent = 'new todo';
+    ul.prepend(li);
+});
 
-content.innerHTML += '<h1>this is content</h1>';
+const button2 = document.querySelector('.append');
 
-const link = document.querySelector('a');
-console.log(link.getAttribute('href'));
-link.setAttribute('href', 'https://www.thenetninja.co.uk/');
-console.log(link.getAttribute('href'));
-
-const paragraphs = document.querySelectorAll('.test > p');
-console.log(paragraphs);
-
-paragraphs.forEach(element => {
-    if(element.textContent.includes('error')) {
-        element.classList.add('error');
-    } else if(element.textContent.includes('success')) {
-        element.classList.add('success');
-    }
+button2.addEventListener('click', () => {
+    const li = document.createElement('li');
+    li.textContent = 'new todo';
+    ul.append(li);
 });
